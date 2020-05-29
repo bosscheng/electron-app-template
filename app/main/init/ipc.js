d9611ac18baa36c61f682484f3411ffaac1f45ab
bosscheng1210 <25022549@qq.com>
2020-05-29 15:55:46 +0800

    Update ipc.js

--- app/main/init/ipc.js
+++ app/main/init/ipc.js
@@ -9,7 +9,49 @@ const {ipcMain, Menu, shell, app: electronApp} = require('electron');
 
 module.exports = (app => {
 
-    ipcMain.on('weblog', async (log) => {
-        app.getLogger('')
+    ipcMain.on('weblog', async (event, args) => {
+        app.getLogger('webLogger').info(args);
     });
+
+    ipcMain.handle('switch-language', async (event, args) => {
+        await app.setLocale(args);
+    });
+
+    ipcMain.on('toggle-maximize', () => {
+        app.mainWindow.isMaximized() ? app.mainWindow.unmaximize() : app.mainWindow.maximize()
+    });
+
+    ipcMain.handle('update-check', async () => {
+        await app.updator.check();
+    });
+
+    ipcMain.handle('update-retry-unzip', async () => {
+        await app.updator.unzipAndReinstall();
+    });
+
+
+    ipcMain.handle('update-start', async (event, args) => {
+        await app.updator.update(args);
+    });
+
+    ipcMain.on('print-to-pdf', (event, args) => {
+        event.sender.printToPDF({
+            landscape: false,
+            marginsType: true,
+            printBackground: true,
+            printSelectionOnly: false,
+            pageSize: "A4"
+        }).then((data) => {
+            const downloadPath = electronApp.getPath("downloads");
+            const filePath = path.join(downloadPath, `${+new Date}.pdf`);
+            fs.writeFile(filePath, data, (error) => {
+                if (error) {
+                    app.logger.warn(error);
+                    return;
+                }
+
+                shell.openExternal(`file://${filePath}`);
+            });
+        })
+    })
 });
